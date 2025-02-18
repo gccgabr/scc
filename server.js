a14a5b1cf48811ccfe53395eaae4ddc1d8147ee8
Gabriel Costa Chaves <gccgabr@pm.me>
2025-02-18 19:42:44 -0300

    More testing.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.use(cors());
+
+//app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/api/data", (req, res) => {
 	const data = {
