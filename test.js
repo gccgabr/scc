4644dcb859100e3f8c6343c32a0365dc6a9c813f
Gabriel Costa Chaves <gccgabr@pm.me>
2025-02-18 22:28:37 -0300

    This is the test sketch.

--- test.js
+++ test.js
@@ -3,9 +3,18 @@
 const db = require("./utils/database");
 
 db.open();
-db.queryAll("Servant").then(rows => {
-	console.log(rows);
+//db.queryAll("Servant").then(rows => {
+//	console.log(rows);
+//});
+//db.addEntry("Servant", ["00000000000", "Pessoa", "000000000000000", "test@test.com", "k3y", 1]);
+db.queryByAttribute("Servant", "name", "Pessoa").then(row => {
+	console.log(row)
 });
+//db.alterAttributeValue("Servant", "name", "Criatura", "cpf", "00000000000");
+db.queryByAttribute("Servant", "name", "Criatura").then(row => {
+	console.log(row)
+});
+db.addEntry("Servant", ["00000000001", "Pessoa", "000000000000000", "test@test.com", "k3y", 1]);
 db.queryByAttribute("Servant", "name", "Pessoa").then(row => {
 	console.log(row)
 });
