2aff4d852e02db599364794d129e83492a50394c
Gabriel Costa Chaves <gccgabr@pm.me>
2025-02-26 13:29:18 -0300

    I removed the inner return statements from promisses resolving method executions, and made their resolved values available and treated on the calling method, so that they are returned to outer invocations.

--- model/servant.js
+++ model/servant.js
@@ -6,45 +6,38 @@ const tableName = "Servant";
 class Servant {
 	// Adicionar novo servidor.
 	addNewServant(cpf, name, phone, email, hashedPassword, isAdmin) {
-		(async() => {
-			await db.open();
-			await db.addEntry(tableName, [cpf, name, phone, email, hashedPassword, isAdmin]);
-			await db.close();
-		})();
+		db.open();
+		db.addEntry(tableName, [cpf, name, phone, email, hashedPassword, isAdmin]);
+		db.close();
 	}
 
 	/* Métodos para leitura. */
 	queryByCPF(cpf) {
 		db.open();
-		db.queryByAttribute(tableName, "cpf", cpf).then(rows => {
-			return rows;
-		});
+		let rows = db.queryByAttribute(tableName, "cpf", cpf);
 		db.close();
+		return rows;
 	}
 
 	queryByName(name) {
 		db.open()
-		db.queryByAttribute(tableName, "name", name).then(rows => {
-			return rows;
-		});
+		let rows = db.queryByAttribute(tableName, "name", name);
 		db.close();
+		return rows;
 	}
 
 	queryByPhoneNumber(phone) {
 		db.open();
-		db.queryByAttribute(tableName, "phone", phone).then(rows => {
-			return rows;
-		});
+		let rows = db.queryByAttribute(tableName, "phone", phone);
 		db.close();
+		return rows;
 	}
 
 	queryByEmailAddress(email) {
 		db.open();
-		db.queryByAttribute(tableName, "email", email).then(rows => {
-			console.log(rows);
-			return rows;
-		});
+		let rows = db.queryByAttribute(tableName, "email", email);
 		db.close();
+		return rows;
 	}
 
 	queryByPrivilege(isAdmin) {
@@ -65,11 +58,18 @@ class Servant {
 
 let srv = new Servant();
 //srv.addNewServant("00000000002", "Pessoa", "000000000000000", "test@test.com", "h4sh3d", 1);
-//srv.queryByCPF("00000000001");
-//srv.queryByName("Pessoa");
-//srv.queryByPhoneNumber("000000000000000");
-//srv.queryByEmailAddress("test@test.com");
+srv.queryByCPF("00000000002").then((rows) => {
+	console.log(rows);
+});
+srv.queryByName("Pessoa").then((rows) => {
+	console.log(rows);
+});
+srv.queryByPhoneNumber("000000000000000").then((rows) => {
+	console.log(rows);
+});
+srv.queryByEmailAddress("test@test.com").then((rows) => {
+	console.log(rows);
+});
 srv.queryByPrivilege(1).then((rows) => {
 	console.log(rows);
 });
-//srv.queryByPrivilege(1);
