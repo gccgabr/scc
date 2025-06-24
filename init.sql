e9d60fd4ddff63c11bbaedd8bda63204d68edfcb
Gabriel Costa Chaves <gccgabr@pm.me>
2025-06-24 09:03:13 -0300

    Definição das tabelas do banco de dados.

--- init.sql
+++ init.sql
@@ -1,6 +1,5 @@
 /* Rotina de inicialização do banco de dados do SCC. */
 
-
 -- Usuário.
 CREATE TABLE User(
 	cpf CHARACTER(11) PRIMARY KEY,
@@ -19,4 +18,22 @@ CREATE TABLE Section(
 );
 
 -- Chave.
+CREATE TABLE Key(
+	code VARCHAR(3) PRIMARY KEY,
+	room_name VARCHAR(30) NOT NULL,
+	status INTEGER NOT NULL,
+	section_code INTEGER,
+	FOREIGN KEY(section_code) REFERENCES Section(code)
+);
+
 -- Empréstimo.
+CREATE TABLE Loan(
+	code INTEGER PRIMARY KEY,
+	timestamp DATATIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	key_code VARCHAR(3),
+	section_code INTEGER,
+	user_cpf CHARACTER(11),
+	FOREIGN KEY(key_code) REFERENCES Key(code),
+	FOREIGN KEY(section_code) REFERENCES Section(code),
+	FOREIGN KEY(user_cpf) REFERENCES User(cpf)
+);
