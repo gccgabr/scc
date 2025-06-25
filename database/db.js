3a65d89efabed827f1e9f2f7de32f6e81324061c
Gabriel Costa Chaves <gccgabr@pm.me>
2025-06-25 09:12:40 -0300

    Módulo gestão do banco de dados.

--- database/db.js
+++ database/db.js
@@ -1,11 +1,40 @@
 "use strict";
 
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
 let dbConnection = null;
 
 async function getDB() {
 	if (!dbConnection) {
-		dbConnection = await open("./scc.db");
+		dbConnection = await open({
+			filename: "./scc.db",
+			driver: sqlite3.Database
+		});
 		console.log("SQLite database connection opened.");
 	}
 	return dbConnection;
 }
+async function closeDB() {
+	if (!dbConnection) {
+		console.log("No database connection open.");
+	} else {
+		dbConnection.close();
+		console.log("Database connection closed.");
+	}
+}
+
+//async function getDB() {
+//	if (!dbConnection) {
+//		dbConnection = await new sqlite3.Database("./scc.db");
+//		console.log("SQLite database connection opened.");
+//	}
+//	return dbConnection;
+//}
+//
+//export class File{};
+
+module.exports = {
+	getDB,
+	closeDB
+};
