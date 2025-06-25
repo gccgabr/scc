0e0fd2ff1fee7b57d0fde1a05456f11bd7296f13
Gabriel Costa Chaves <gccgabr@pm.me>
2025-06-25 12:56:04 -0300

    Atualização de dados de chave bem sucedida.

--- services/key.js
+++ services/key.js
@@ -73,7 +73,30 @@ const getKeyByStatus = async (keyStatus) => {
 //		console.log(error);
 //	});
 
-getKeyByStatus(0)
+
+
+// Update.
+const updateKey = async (code, newCode, newRoomName, newStatus, newSectionCode) => {
+	let db = await getDB();
+	let query = "UPDATE Key SET code = ?, room_name = ?, status = ?, section_code = ? WHERE code = ?";
+	let query_values = [newCode, newRoomName, newStatus, newSectionCode, code];
+	return await db.run(query, query_values, (err) => {
+		if (err) {
+			return err;
+		}
+		return true;
+	});
+}
+
+//updateKey(0, 1, "SALA 1", 1, 0)
+//	.then(result => {
+//		console.log(result);
+//	})
+//	.catch(error => {
+//		console.log(error);
+//	});
+
+getKeyByStatus(1)
 	.then(result => {
 		console.log(result);
 	})
@@ -81,57 +104,6 @@ getKeyByStatus(0)
 		console.log(error);
 	});
 
-
-//// Read.
-//const getSectionByUserCpf = async (user_cpf) => {
-//	let db = await getDB();
-//	let query = "SELECT * FROM Section WHERE user_cpf = ?";
-//	let query_values = [user_cpf];
-//	return await db.all(query, query_values, (err, rows) => {
-//		if (err) {
-//			return err;
-//		}
-//		return rows;
-//	});
-//};
-//
-//const getSectionByName = async (name) => {
-//	let db = await getDB();
-//	let query = "SELECT * FROM Section WHERE name = ?";
-//	let query_values = [name];
-//	return await db.all(query, query_values, (err, rows) => {
-//		if (err) {
-//			return err;
-//		}
-//		return rows;
-//	});
-//};
-//
-//const getSectionByCode = async (code) => {
-//	let db = await getDB();
-//	let query = "SELECT * FROM Section WHERE code = ?";
-//	let query_values = [code];
-//	return await db.all(query, query_values, (err, rows) => {
-//		if (err) {
-//			return err;
-//		}
-//		return rows;
-//	});
-//}
-//
-//// Update.
-//const updateSection = async (code, newName, newUserCpf) => {
-//	let db = await getDB();
-//	let query = "UPDATE Section SET name = ?, user_cpf = ? WHERE code = ?";
-//	let query_values = [newName, newUserCpf, code];
-//	return await db.run(query, query_values, (err) => {
-//		if (err) {
-//			return err;
-//		}
-//		return true;
-//	});
-//}
-//
 //// Delete.
 //const deleteSection = async (code) => {
 //	let db = await getDB();
