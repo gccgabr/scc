5dad463805971acc10cfd1e9557ed912b853c40b
Gabriel Costa Chaves <gccgabr@pm.me>
2025-06-25 18:38:11 -0300

    Configuração preliminar do subprograma principal.

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Configuração básica do Express.
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Middlewares.
+
+// Rotas.
+
+// Início do sistema.
+app.listen(port, () => {
+	console.log("Escutando em on http://localhost:", port);
+});
