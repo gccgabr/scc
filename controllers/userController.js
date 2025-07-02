2705e59882e93d3eda0e71ea8ba10c6ab405d06f
Gabriel Costa Chaves <gccgabr@pm.me>
2025-07-02 11:29:30 -0300

    Construção das funções de consulta de usuários, pelo controlador de usuários.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,56 @@
-/* Este subprograma deve ser responsável pela validação e pelo tratamento dos dados recebidos da API. */
+/* Este subprograma deve ser responsável pela validação e pelo tratamento dos
+ * dados recebidos da API. OBS.: A senha codificada presente no banco de dados
+ * não é ordinariamente retornada pelas funções consulta. */
 "use strict";
 
+const USER = require("../services/user.js");
+
+/* Tratamento e validação dos dados recebidos para realização de operações
+ * CRUD. */
+// Create.
+//const createNewUser = async (cpf, name, email, phone, role, hashed_password) => {
+//};
+
+// Read.
+// Consulta de usuário por meio de CPF.
+const getUserByCpf = async (cpf) => {
+	USER.getUserByCpf(cpf)
+		.then(result => {
+			if (result.length > 0) {
+				delete result[0]["hashed_password"]; // Remoção da senha codificada.
+				return result[0];
+			}
+			return {};
+		})
+		.catch(error => {
+			return error;
+		});
+};
+
+// Consulta de usuários por meio de nome.
+const getUsersByName = async (name) => {
+	USER.getUsersByName(name)
+		.then(result => {
+			if (result.length > 0) {
+				result.forEach((obj) => { delete obj.hashed_password });
+			}
+			return result;
+		})
+		.catch(error => {
+			return error;
+		});
+};
+					
+// Consulta de usuários por meio de função.
+const getUsersByRole = async (role) => {
+	USER.getUsersByRole(role)
+		.then(result => {
+			if (result.length > 0) {
+				result.forEach((obj) => { delete obj.hashed_password });
+			}
+			return result;
+		})
+		.catch(error => {
+			return error;
+		});
+};
