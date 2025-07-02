f57d2d205fbfd0dbd711c1154ec956de52e96883
Gabriel Costa Chaves <gccgabr@pm.me>
2025-07-02 09:58:02 -0300

    Consulta de usuário por meio do CPF, pela API.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,13 @@
 "use strict";
 
-const { getAllUsers } = require("../services/user.js");
+/* Em cada rota, deve-se invocar a função cabível, presente na construção do
+ * controlador do usuário. A função controladora deverá intermediar o acesso
+ * aos dados no banco de dados, e tratar alguns aspectos de segurança. */
+
+const {
+	getAllUsers,
+	getUserByCpf
+} = require("../services/user.js");
 
 const express = require("express");
 const app = express();
@@ -10,7 +17,7 @@ const port = 3000;
 app.use(express.json());
 
 /* Rotas. (CRUD) */
-// Read.
+// Obter um vetor de todos os usuários.
 app.get("/api/user", (req, res) => {
 	console.log("GET /api/user request.");
 	getAllUsers()
@@ -22,6 +29,20 @@ app.get("/api/user", (req, res) => {
 		});
 });
 
+// Consultar um usuário por meio do CPF.
+app.get("/api/user/:cpf", (req, res) => {
+	console.log("GET /api/user/ request.");
+	getUserByCpf("11111111111")
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
+});
+
+
+/* Escuta do servidor. */
 app.listen(port, () => {
 	console.log("Escutando na porta " + port + ".");
 });
