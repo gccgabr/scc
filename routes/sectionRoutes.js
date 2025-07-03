4368af142448a3106522722ae0fe18df693291b2
Gabriel Costa Chaves <gccgabr@pm.me>
2025-07-03 10:08:40 -0300

    Rotas para consulta de seções.

--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -27,22 +27,60 @@ app.post("/api/section/new", (req, res) => {
 app.get("/api/section/user-cpf/:user-cpf", (req, res) => {
 	if (!req.body) return res.sendStatus(400);
 	console.log("GET /api/section/user-cpf/" + req.params.userCpf + " request.");
+
+	SECTION_CONTROLLER.getSectionByUserCpf(
+			req.params.userCpf
+		)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
 });
 
 // Consultar seção por nome.
 app.get("/api/section/name/:name", (req, res) => {
 	if (!req.body) return res.sendStatus(400);
 	console.log("GET /api/section/name/:name" + req.params.name + " request.");
+
+	SECTION_CONTROLLER.getSectionByName(
+			req.body.name
+		)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
 });
 
 // Consultar seção por código.
 app.get("/api/section/code/:code", (req, res) => {
 	if (!req.body) return res.sendStatus(400);
 	console.log("GET /api/section/code/:code" + req.params.code + " request.");
+
+	SECTION_CONTROLLER.getSectionByCode(
+			req.params.code
+		)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
 });
 
-// Consultar todas as seções.
+// Consultar todos os códigos das seções.
 app.get("/api/section/all", (req, res) => {
 	if (!req.body) return res.sendStatus(400);
 	console.log("GET /api/section/all request.");
+
+	SECTION_CONTROLLER.getAllSectionCodes()
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
 });
