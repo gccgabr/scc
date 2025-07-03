27214aadeae65590872101e272f12a36db6d79fa
Gabriel Costa Chaves <gccgabr@pm.me>
2025-07-03 09:31:07 -0300

    Rota para criação de nova chave.

---
+++ routes/keyRoutes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const express = require("express");
+const KEY_CONTROLLER = require("../controllers/key.js");
+
+/* Middleware configuration. */
+app.use(express.json());
+
+/* Rotas. */
+// Criação de nova chave.
+app.post("/api/key/new", (req, res) => {
+	if (!req.body) return res.sendStatus(400);
+	console.log("POST /api/key/new");
+	
+	KEY_CONTROLLER.createNewKey(
+			req.body.code,
+			req.body.roomName,
+			req.body.keyStatus,
+			req.body.sectionCode
+		)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
+});
