f49d4376666754068c06a3d7dbc4406eb42a7c92
Gabriel Costa Chaves <gccgabr@pm.me>
2025-07-03 10:48:48 -0300

    Rotas para atualização de status de empréstimo e para deleção de empréstimo.

--- routes/loanRoutes.js
+++ routes/loanRoutes.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // Criar nova seção.
 app.post("/api/loan/new", (req, res) => {
 	if (!req.body) return res.sendStatus(400);
-	console.log("POST /api/loan/new");
+	console.log("POST /api/loan/new request.");
 
 	LOAN_CONTROLLER.createNewLoan(
 			req.body.keyCode,
@@ -70,6 +70,36 @@ app.get("/api/loan/code/:code", (req, res) => {
 app.get("/api/loan/all", (req, res) => {
 	console.log("GET /api/loan/all request.");
 	LOAN_CONTROLLER.getAllLoans()
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
+});
+
+// Atualizar status de empréstimo.
+app.put("/api/loan/overdue", (req, res) => {
+	if (!req.body) return res.sendStatus(400);
+	console.log("PUT /api/loan/overdue request.");
+	LOAN_CONTROLLER.setLoanOverdue(
+			req.body.code
+		)
+		.then(result => {
+			res.send(result);
+		})
+		.catch(error => {
+			res.send(error);
+		});
+});
+
+// Atualizar status de empréstimo.
+app.delete("/api/loan/del", (req, res) => {
+	if (!req.body) return res.sendStatus(400);
+	console.log("DELETE /api/loan/new request.");
+	LOAN_CONTROLLER.deleteLoan(
+			req.body.code
+		)
 		.then(result => {
 			res.send(result);
 		})
